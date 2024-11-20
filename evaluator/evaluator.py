293471e038561a805fc82b23a1fdb7cc69e02b88
abdullahsestek <abdullahsestek@gmail.com>
2024-11-20 08:00:29 +0000

    Evaluator class added

--- evaluator/evaluator.py
+++ evaluator/evaluator.py
@@ -9,6 +9,15 @@ import google.generativeai as genai
 from trulens.apps.custom import instrument
 from trulens.core import TruSession
 from pinecone.grpc import PineconeGRPC as Pinecone
+import numpy as np
+from trulens.core import Feedback
+from trulens.core import Select
+from trulens.providers.openai import OpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
+from trulens.providers.langchain import Langchain
+from langchain_community.llms import OpenAI
+from trulens.apps.custom import TruCustomApp
+
 
 
 
@@ -20,14 +29,21 @@ load_dotenv()
 
 class RAG_eval:
     
-    def __init__(self,retriever,generator, db_name, reset_database = false):
+    def __init__(self,retriever,generator, db_name, reset_database = false, cos = true, provider_name="Gemini", version = "v1.0"):
         self.db_name = db_name
         self.reset_database = reset_database
+        self.version = version
         self.retriever = retriever # Retriever object
         self.generator = generator  # Generator object
         self.session = self._initiate_session()
         self.index = self._initiate_db()
-    
+        self.cos = cos   # For chain of thought evaluation. More costly 
+        self.provider_name = provider_name
+        self.provider = self._initiate_provider()
+        self.feedback = self._initiate_feedback()
+        self.app = self._initiate_app()
+        
+        
     def _initiate_session( self):
         session = TruSession()
         if(self.reset_database):
@@ -40,6 +56,13 @@ class RAG_eval:
         index = pc.Index(self.db_name)
         return index
 
+    def _initiate_app(self):
+        return TruCustomApp(
+            self,
+            app_name=f"RAG-{self.provider_name}-{self.db_name}-{"Cos" if self.cos else""}}",
+            app_version=self.version,
+            feedbacks=self.feedback,
+            )
 
     # Instrumented Retrieval methods
     @instrument
@@ -71,6 +94,75 @@ class RAG_eval:
         response = self.generate(question, context)
         
         return response
+
+    def _initiate_feedback(self):
+        #Adhere to trulens version 1.2.6 https://www.trulens.org/reference/trulens/providers/openai/#trulens.providers.openai.OpenAI
+        if self.cos :
+            f_groundedness = (
+                Feedback(
+                    provider.groundedness_measure_with_cot_reasons, name="Groundedness-COS"
+                )
+                .on(Select.RecordCalls.retrieve.rets.collect())
+                .on_output()
+            )
+
+            f_answer_relevance = (
+                Feedback(provider.relevance_with_cot_reasons, name="Answer Relevance-COS")
+                .on_input()
+                .on_output()
+            )
+
+            f_context_relevance = (
+                Feedback( provider.context_relevance_with_cot_reasons, name="Context Relevance-COS").on_input()
+                .on(Select.RecordCalls.retrieve.rets[:])
+                .aggregate(np.mean)  # choose a different aggregation method if you wish
+            )
+
+        else:
+            #In case Chain of thought reasons were not asked to be provided  (cos= false)
+            f_groundedness = (
+                Feedback(
+                    provider.groundedness_measure_with_cot_reasons, name="Groundedness - COS"
+                )
+                .on(Select.RecordCalls.retrieve.rets.collect())
+                .on_output()
+            )
+
+            f_answer_relevance = (
+                Feedback(provider.relevance, name="Answer Relevance")
+                .on_input()
+                .on_output()
+            )
+
+            f_context_relevance = (
+                Feedback( provider.context_relevance, name="Context Relevance").on_input()
+                .on(Select.RecordCalls.retrieve.rets[:])
+                .aggregate(np.mean)  # choose a different aggregation method if you wish
+            )
+
+        return [f_groundedness, f_answer_relevance, f_context_relevance]
+
+        def _initiate_provider(self):
+            if self.provider == "Gemini":
+                llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash-002",api_key=os.getenv("GEMINI_API_KEY"))
+                provider = Langchain(chain = llm)
+                return provider
+            elif self.provider == "OpenAI":
+                return OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+        
+
+        def run(self, questions):
+            with self.app as recording:
+                for eval in questions:
+                    self.query(eval["question"])
+
+
+
+
+
+
+
+    
 
 
 
--- main.py
+++ main.py
@@ -8,22 +8,22 @@ import numpy as np
 from chunker.chunk_repo import chunk_repo
 from utils.utils import process_duplicates
 from utils.utils import FastChunkURLMapper
-from evaluator.evaluator import RAG_eval
+# from evaluator.evaluator import RAG_eval
 
 
 
 
-query = "How can a complaint be lodged against a private health facility, and what are the follow-up requirements if the facility’s professional licenses are revoked due to the complaint?"
-ret = cohere_retriever()
+query = "for a sector that is fee-exempt from a staff license renewal? what is the required document to get a good standing certificates for its staff ?"
+ret = gemini_retriever()
 
 chunks = ret.get_Chunks(query)
-# gen = gemini_generator()
-gen = cohere_generator()
-# response = gen.generate(query, chunks)
+gen = gemini_generator()
+# gen = cohere_generator()
+response = gen.generate(query, chunks)
 
-eval = RAG_eval(ret,gen,"gemini-test")
-response = eval.query(query)
+# eval = RAG_eval(ret,gen,"gemini-test")
+# response = eval.query(query)
 
+print(chunks)
 print(response)
-
 
